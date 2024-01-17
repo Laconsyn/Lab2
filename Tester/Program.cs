6f73be562a4c407d92eba0abc2bd5dbc70425981
Laconsyn <laconsyn@gmail.com>
2024-01-17 08:23:26 -0700

    Added a test of addition.

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -5,6 +5,15 @@ void test1()
     Console.WriteLine(Class1.Evaluate("5"));
 }
 
+void test2()
+{
+    if (Class1.Evaluate("5+5") != 10)
+    {
+        Console.WriteLine("Error in simple addition!");
+    }
+}
+
 test1();
+test2();
 
 Console.Read();
